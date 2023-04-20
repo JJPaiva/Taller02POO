8b1f148e3628520c88fbf8fbe453231c9d53c9e8
Juan Paiva <j.paiva01@ufromail.cl>
2023-04-20 09:33:24 -0400

    Update Taller02.java

--- Taller02.java
+++ Taller02.java
@@ -1,9 +1,13 @@
+
 import java.util.Scanner;
 
 public class Taller02 {
 
+    public static int[][] matriz= new int[10][3];
+
     public static void main(String[] args) {
-        
+        EditarDisponibilidad(1234123, 10);
+        System.out.println(matriz[0][0]);
     }
 
     public static String Input() {
@@ -31,8 +35,19 @@ public class Taller02 {
         return stringToInt(formatoInput(Input()));
     }
 
-    public static void matriz(String[] args) {
-        
+    public static int[] valores(int val1, int val2) {
+        int[] arr= new int[2];
+        arr[0]=val1;
+        arr[1]=val2;
+        return arr;
+    }
+
+    public static void EditarDisponibilidad(int val1, int val2) {
+        try {
+            matriz[val1-1][0]=val2;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Se ha ingresado un numero de habitación inexixstente");
+        }
     }
 
 
